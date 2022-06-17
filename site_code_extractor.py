32915b7846bc9152bb1baa67534b934ead679052
Mateusz laptop <malicki9mateusz4@gmail.com>
2022-06-17 12:54:41 +0200

    Some comments had been added

--- site_code_extractor.py
+++ site_code_extractor.py
@@ -6,14 +6,19 @@ options.add_argument("--headless") #It prevent web browser window from appearing
 driver = webdriver.Chrome(executable_path="C:/Users/Maliccy/AppData/Local/Programs/chromedriver.exe", chrome_options = options)
 # driver = webdriver.Chrome()
 
+print("***************************\n")
 url = input("Podaj adres strony: ")
 driver.get(url)
 fixed_url = wac(url).address_generator()
 
 site_source = driver.page_source
 save_file_path = "D:/Użytkownicy/Maliccy/Dokumenty/Mateusz/PROGRAMOWANIE/Python/Projekty/WebCodeEXTR/Sites_codes/site_source_of_" + str(fixed_url) + ".txt"
-print(save_file_path)
+
 
 with open(save_file_path, "w", encoding="utf-8") as ss_file:
     ss_file.write(str(site_source))
+
+print("***************************\n")
+print("Gotowe!")
+print("Kod strony został zapisany jako: " + save_file_path)
 
